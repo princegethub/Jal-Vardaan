eb0f3e53f8acfaa0ab86a0da55393526bbf6c0e5
Prince Mishra <pm618209@gmail.com>
2024-11-24 17:02:02 +0530

    Save local changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,8 @@ import { useState } from "react";
 
 import "./App.css";
 
-import Button from './components/ui/Button';
+import Button from "./components/ui/Button";
 import TopHeader from "./components/home components/topHeader";
-
-
-
 
 import Navbar from "./components/home components/Navbar";
 import HeroPage from "./components/home components/HeroPage";
@@ -14,23 +11,22 @@ import About from "./components/home components/About";
 import BetterTommorow from "./components/home components/BetterTommorow";
 import ZigZag from "./components/home components/ZigZag";
 import Footer from "./components/home components/Footer";
-
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-
-  
   return (
     <>
 
-    <TopHeader/>
-  
 
-    <Navbar/>
-    <HeroPage/>
-    <About/>
-    <BetterTommorow/>
-    <ZigZag/>
-    <Footer/>
+      <TopHeader />
+      <Navbar />
+      <HeroPage />
+
+    
+      <About />
+      <BetterTommorow />
+      <ZigZag />
+      <Footer />
     </>
   );
 }
