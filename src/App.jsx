915269dd2a739c1b33cdc679c7016bc58c7823c8
Prince Mishra <pm618209@gmail.com>
2024-11-27 18:11:16 +0530

    Server Stop Problem Solve

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,13 @@ import React from 'react'
 import Navbar from './components/GPS Components/Navbar_GPS'
 import Footer_PHED from './components/FooterLoginKeBaadWala'
 import Dashboard_Gps from './components/GPS Components/Dashboard_Gps'
-import ManageConsumerPage from './components/GPS Components/ManageConsumerPage'
 
 function App() {
   return (
     <div>
       <Navbar/>
       {/* <Dashboard_Gps/> */}
-      <ManageConsumerPage/>
+
       <Footer_PHED/>
     </div>
   )
